ca4f00ea5b7a158235479c6bce751f692c832186
George Vasiliadis <geor.vasiliadis@gmail.com>
2022-06-24 09:51:27 +0300

    Added better return values

--- CleanEmonPopulator/buffer.py
+++ CleanEmonPopulator/buffer.py
@@ -16,16 +16,18 @@ class AutoBuffer:
 
         self._count += 1
         if self._count >= self._capacity:
-            self._clear()
-            cleared = True
+            cleared = self._clear()
 
         return cleared
 
     def _clear(self):
         for document, data_list in self.data.copy().items():
+            print(f"Sending {len(data_list)} records to {document}...")
             self.db_adapter.append_energy_data(*data_list, document=document)
             del self.data[document]
         self._count = 0
+
+        return bool(self.data)  # Empty dictionary == False, otherwise True
 
     def append_data(self, energy_data: EnergyData, document: str):
         if document not in self.data:
