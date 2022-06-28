cdfff164c3ca0018c97aa255bc1c537aa9401b43
George Vasiliadis <geor.vasiliadis@gmail.com>
2022-06-28 18:02:51 +0300

    Introduced CLI options. Setup in __init__ is deprecated. The user should take care of generating config files

--- CleanEmonPopulator/__init__.py
+++ CleanEmonPopulator/__init__.py
@@ -2,27 +2,23 @@
 
 import os
 
-from .EmonPiAdapter import EmonPiAdapter
 from CleanEmonCore.dotfiles import get_dotfile
+
+from .EmonPiAdapter import EmonPiAdapter
 
 _CONFIG_FILENAME = "clean.cfg"
 _SCHEMA_FILENAME = "schema.json"
-
 
 # Check if config-file lies in directory of execution
 # If not, consider working with the global one
 if os.path.exists(_CONFIG_FILENAME):
     CONFIG_FILE = os.path.abspath(_CONFIG_FILENAME)
 else:
-    # If there is no global file, generate a new one
-    from .setup import generate_config
-    CONFIG_FILE = get_dotfile(_CONFIG_FILENAME, generate_config)
+    CONFIG_FILE = get_dotfile(_CONFIG_FILENAME)
 
 # Check if schema-file lies in directory of execution
 # If not, consider working with the global one
 if os.path.exists(_SCHEMA_FILENAME):
     SCHEMA_FILE = os.path.abspath(_SCHEMA_FILENAME)
 else:
-    # If there is no global file, generate a new one
-    from .setup import generate_schema
-    SCHEMA_FILE = get_dotfile(_SCHEMA_FILENAME, generate_schema)
+    SCHEMA_FILE = get_dotfile(_SCHEMA_FILENAME)
